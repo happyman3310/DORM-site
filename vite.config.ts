7ba0b3ac0b9a502fd23c775d0dd852da2ff30cac
happyman3310 <64700323+happyman3310@users.noreply.github.com>
2025-06-10 02:35:14 +0300

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  server: {
+    port: 3000,
+    // Добавляем настройку для SPA
+    historyApiFallback: true
+  },
+  preview: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
+});
