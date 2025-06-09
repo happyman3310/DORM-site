99a127d21185220b61b51af95e1b69e57872665f
happyman3310 <64700323+happyman3310@users.noreply.github.com>
2025-06-10 02:13:27 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import { ThemeProvider } from '@emotion/react'
 import App from './App.tsx'
 import './index.css'
+
+const theme = extendTheme({
+  // ваши настройки темы
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <ThemeProvider theme={theme}>
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
