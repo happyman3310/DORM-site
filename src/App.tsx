9bc090c462fc7afd9be74aecc3daf14e931cf968
happyman3310 <64700323+happyman3310@users.noreply.github.com>
2025-06-10 02:46:29 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,26 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import { motion } from "framer-motion";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomePage from "./pages/HomePage";
-import ProductPage from "./pages/ProductPage";
-import TeamPage from "./pages/TeamPage";
-import LegalPage from "./pages/LegalPage";
-import { LanguageProvider } from "./context/LanguageContext";
-import CookieConsent from "./components/CookieConsent";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { FC } from 'react';
+import Home from './pages/Home';
+import Mission from './pages/Mission';
+import Product from './pages/Product';
+import Team from './pages/Team';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
-export default function App() {
-  const [showCookieConsent, setShowCookieConsent] = React.useState(false);
-
-  React.useEffect(() => {
-    const hasConsented = localStorage.getItem("cookie-consent");
-    if (!hasConsented) {
-      setShowCookieConsent(true);
-    }
-  }, []);
-
-  const handleCookieConsent = () => {
-    localStorage.setItem("cookie-consent", "true");
-    setShowCookieConsent(false);
-  };
-
+const App: FC = () => {
   return (
-    <LanguageProvider>
-      <div className="dark min-h-screen bg-background text-foreground">
-        <Header />
-        <main>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/product" component={ProductPage} />
-            <Route path="/team" component={TeamPage} />
-            <Route path="/legal" component={LegalPage} />
-          </Switch>
-        </main>
-        <Footer />
-        {showCookieConsent && (
-          <motion.div
-            initial={{ y: 100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.6 }}
-            className="fixed bottom-0 left-0 right-0 z-50 bg-content1 p-4 shadow-lg"
-          >
-            <CookieConsent onAccept={handleCookieConsent} />
-          </motion.div>
-        )}
-      </div>
-    </LanguageProvider>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mission" element={<Mission />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
-}
+};
+
+export default App;
